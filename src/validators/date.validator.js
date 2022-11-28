643c898757cdadf23991df49a06346e7a28788f0
Xavier DARMET <jy.pyxx@gmail.com>
2022-11-28 15:44:02 +0100

    fix(datepicker): allow array for date validator

--- src/validators/date.validator.js
+++ src/validators/date.validator.js
@@ -6,7 +6,17 @@ dayjs.extend(isSameOrAfter)
 dayjs.extend(isSameOrBefore)
 
 export const minDate = min =>
-  helpers.withParams({ type: 'minDate', min }, value => !helpers.req(value) || dayjs(value).isSameOrAfter(min, 'day'))
+  helpers.withParams({ type: 'minDate', min }, value => {
+    if (Array.isArray(value)) {
+      return !helpers.req(value[0]) || dayjs(value[0]).isSameOrAfter(min, 'day')
+    }
+    return !helpers.req(value) || dayjs(value).isSameOrAfter(min, 'day')
+  })
 
 export const maxDate = max =>
-  helpers.withParams({ type: 'maxDate', max }, value => !helpers.req(value) || dayjs(value).isSameOrBefore(max, 'day'))
+  helpers.withParams({ type: 'maxDate', max }, value => {
+    if (Array.isArray(value)) {
+      return !helpers.req(value[1]) || dayjs(value[1]).isSameOrBefore(max, 'day')
+    }
+    return !helpers.req(value) || dayjs(value).isSameOrBefore(max, 'day')
+  })
